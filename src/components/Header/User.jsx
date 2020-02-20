89eb83ec52d55a7f84e86d2cf3060a9b6c0b589e
Kirillova <keira.kirillova@gmail.com>
2020-02-20 14:38:06 +0200

    add log out window to user component

--- src/components/Header/User.jsx
+++ src/components/Header/User.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import AppContextHOC from "./../HOC/AppContextHOC";
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 class User extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownOpen: false
+    }
+  }
+
+  toggleDropdownOpen = () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }))
+  };
+
+  handleLogOut = () => {
+    console.log('Log out')
+  };
+
   render() {
+    const {dropdownOpen} = this.state;
     const {user} = this.props;
     return (
-      <img
-        className="rounded-circle"
-        width="40"
-        src={`https://secure.gravatar.com/avatar/${user.avatar.gravatar.hash}.jpg?s=64`}
-        alt="User" />
+      <Dropdown isOpen={dropdownOpen} toggle={this.toggleDropdownOpen}>
+        <DropdownToggle
+          tag="div"
+          data-toggle="dropdown"
+          aria-expanded={dropdownOpen}
+        >
+          <img
+            className="rounded-circle"
+            width="40"
+            src={`https://secure.gravatar.com/avatar/${user.avatar.gravatar.hash}.jpg?s=64`}
+            alt="User" />
+        </DropdownToggle>
+        <DropdownMenu right>
+          <DropdownItem onClick={this.handleLogOut}>Выйти</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
     )
   }
 }
