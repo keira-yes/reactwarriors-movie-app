af140f6eb0caa4c233ea01e94f10f7158753a235
Kirillova <keira.kirillova@gmail.com>
2020-03-26 15:35:36 +0200

    add toggle method to add/remove watch list items

--- src/components/Movies/WatchListItem.jsx
+++ src/components/Movies/WatchListItem.jsx
@@ -4,7 +4,8 @@ import CallApi from "./../../api/api";
 import {Bookmark, BookmarkBorder} from '@material-ui/icons';
 
 class WatchListItem extends React.Component {
-  addToWatchList = () => {
+
+  toggleWatchList = () => {
     const {user, session_id, item, getWatchListMovies, toggleModal} = this.props;
     if (session_id) {
       CallApi.post(`/account/${user.id}/watchlist`, {
@@ -14,35 +15,23 @@ class WatchListItem extends React.Component {
         body: {
           media_type: "movie",
           media_id: item.id,
-          watchlist: true
+          watchlist: !this.isWatchListMovie()
         }
       }).then(() => getWatchListMovies(user, session_id));
     } else {toggleModal()}
   };
 
-  removeFromWatchList = () => {
-    const {user, session_id, item, getWatchListMovies} = this.props;
-    if (session_id) {
-      CallApi.post(`/account/${user.id}/watchlist`, {
-        params: {
-          session_id: session_id
-        },
-        body: {
-          media_type: "movie",
-          media_id: item.id,
-          watchlist: false
-        }
-      }).then(() => getWatchListMovies(user, session_id));
-    }
+  isWatchListMovie = () => {
+    const {watchList, item} = this.props;
+    return watchList.findIndex(movie => movie.id === item.id) !== -1;
   };
 
   render() {
-    const {item, watchList} = this.props;
     return (
       <>
-        {watchList.findIndex(movie => movie.id === item.id) === -1 ?
-          <span onClick={this.addToWatchList}><BookmarkBorder/></span> :
-          <span onClick={this.removeFromWatchList}><Bookmark/></span>
+        {this.isWatchListMovie() ?
+          <span onClick={this.toggleWatchList}><Bookmark/></span> :
+          <span onClick={this.toggleWatchList}><BookmarkBorder/></span>
         }
       </>
     )
