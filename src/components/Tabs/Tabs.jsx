bdaca1c3b5549a7cd0adfcae3c63c7a98f1393ef
Kirillova <keira.kirillova@gmail.com>
2020-04-05 20:19:22 +0300

    remove utility classnames

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,78 +1,64 @@
-import React, { useState } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
-import classnames from 'classnames';
+import React from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
 
-const Tabs = (props) => {
-  const [activeTab, setActiveTab] = useState('1');
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const toggle = tab => {
-    if(activeTab !== tab) setActiveTab(tab);
+    this.state = {
+      activeTab: '1'
+    }
+  }
+
+  toggleActive = (tab) => {
+    if (this.state.activeTab !== tab) this.setState({activeTab: tab});
   };
 
-  return (
-    <div>
-      <Nav tabs>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '1' })}
-            onClick={() => { toggle('1'); }}
-          >
-            Детали
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '2' })}
-            onClick={() => { toggle('2'); }}
-          >
-            Видео
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '3' })}
-            onClick={() => { toggle('3'); }}
-          >
-            Актеры
-          </NavLink>
-        </NavItem>
-      </Nav>
-      <TabContent activeTab={activeTab}>
-        <TabPane tabId="1">
-          <Row>
-            <Col sm="12">
-              <h4>Tab 1 Contents</h4>
-            </Col>
-          </Row>
-        </TabPane>
-        <TabPane tabId="2">
-          <Row>
-            <Col sm="6">
-              <Card body>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Go somewhere</Button>
-              </Card>
-            </Col>
-            <Col sm="6">
-              <Card body>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Go somewhere</Button>
-              </Card>
-            </Col>
-          </Row>
-        </TabPane>
-        <TabPane tabId="3">
-          <Row>
-            <Col sm="12">
-              <h4>Tab 3 Contents</h4>
-            </Col>
-          </Row>
-        </TabPane>
-      </TabContent>
-    </div>
-  );
-};
+  render() {
+    const {activeTab} = this.state;
+
+    return (
+      <div>
+        <Nav tabs>
+          <NavItem>
+            <NavLink onClick={() => this.toggleActive('1')}>Детали</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink onClick={() => this.toggleActive('2')}>Видео</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink onClick={() => this.toggleActive('3')}>Актеры</NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={activeTab}>
+          <TabPane tabId="1">
+            <div className="row">
+              <div className="col-sm-12">
+                <h4>Tab 1 Contents</h4>
+              </div>
+            </div>
+          </TabPane>
+          <TabPane tabId="2">
+            <div className="row">
+              <div className="col-sm-6">
+                123
+              </div>
+              <div className="col-sm-6">
+                123
+              </div>
+            </div>
+          </TabPane>
+          <TabPane tabId="3">
+            <div className="row">
+              <div className="col-sm-12">
+                <h4>Tab 3 Contents</h4>
+              </div>
+            </div>
+          </TabPane>
+        </TabContent>
+      </div>
+    );
+  }
+}
 
 export default Tabs;
