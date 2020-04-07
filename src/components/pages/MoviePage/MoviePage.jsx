6110c88358ef0f94403f1ad983a6068082982e13
Kirillova <keira.kirillova@gmail.com>
2020-04-07 18:02:43 +0300

    add loader to movie page

--- src/components/pages/MoviePage/MoviePage.jsx
+++ src/components/pages/MoviePage/MoviePage.jsx
@@ -4,6 +4,7 @@ import AppContextHOC from "../../HOC/AppContextHOC";
 import FavoriteItem from '../../Movies/FavoriteItem';
 import WatchListItem from '../../Movies/WatchListItem';
 import Tabs from '../../Tabs/Tabs';
+import {Loader} from '../../UIComponents/Loader';
 
 class MoviePage extends React.Component {
   constructor(props) {
@@ -12,7 +13,8 @@ class MoviePage extends React.Component {
     this.state = {
       movieDetail: {},
       movieVideos: [],
-      movieCredits: []
+      movieCredits: [],
+      isLoading: true
     }
   }
 
@@ -22,7 +24,7 @@ class MoviePage extends React.Component {
         language: "ru-RU"
       }
     })
-      .then(data => this.setState({movieDetail: data}))
+      .then(data => this.setState({movieDetail: data, isLoading: false}))
   };
 
   getMovieVideos = (id) => {
@@ -48,6 +50,7 @@ class MoviePage extends React.Component {
 
   render() {
     const {
+      isLoading,
       movieDetail,
       movieDetail: {
         poster_path,
@@ -65,38 +68,42 @@ class MoviePage extends React.Component {
     const {movie_id} = this.props.match.params;
 
     return (
-      <div className='container'>
-        <div className="row mt-5">
-          <div className="col-4">
-            <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title}/>
-          </div>
-          <div className="col-8">
-            <h1>{title} {release_date && `(${this.getReleaseYear(release_date)})`}</h1>
-            <ul className="list-group list-group-horizontal-sm mt-3">
-              <li className="list-group-item list-group-item-secondary">рейтинг {vote_average}</li>
-              <li className="list-group-item list-group-item-success">бюджет ${budget}</li>
-              <li className="list-group-item list-group-item-danger">
-                {genres && genres.map(item => <span key={item.id} className="list-item">{item.name}</span>)}
-              </li>
-            </ul>
-            <div className="mt-4">{overview}</div>
-            <div className="mt-4">
-              <FavoriteItem itemId={movie_id}/>
-              <WatchListItem itemId={movie_id}/>
+      <>
+        {isLoading ? <Loader /> :
+          <div className='container'>
+            <div className="row mt-5">
+              <div className="col-4">
+                <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title}/>
+              </div>
+              <div className="col-8">
+                <h1>{title} {release_date && `(${this.getReleaseYear(release_date)})`}</h1>
+                <ul className="list-group list-group-horizontal-sm mt-3">
+                  <li className="list-group-item list-group-item-secondary">рейтинг {vote_average}</li>
+                  <li className="list-group-item list-group-item-success">бюджет ${budget}</li>
+                  <li className="list-group-item list-group-item-danger">
+                    {genres && genres.map(item => <span key={item.id} className="list-item">{item.name}</span>)}
+                  </li>
+                </ul>
+                <div className="mt-4">{overview}</div>
+                <div className="mt-4">
+                  <FavoriteItem itemId={movie_id}/>
+                  <WatchListItem itemId={movie_id}/>
+                </div>
+              </div>
+            </div>
+            <div className="row mt-5">
+              <div className="col-12">
+                <Tabs
+                  movie_id={movie_id}
+                  movieDetail={movieDetail}
+                  movieVideos={movieVideos}
+                  movieCredits={movieCredits}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row mt-5">
-          <div className="col-12">
-            <Tabs
-              movie_id={movie_id}
-              movieDetail={movieDetail}
-              movieVideos={movieVideos}
-              movieCredits={movieCredits}
-            />
-          </div>
-        </div>
-      </div>
+        }
+      </>
     )
   }
 }
