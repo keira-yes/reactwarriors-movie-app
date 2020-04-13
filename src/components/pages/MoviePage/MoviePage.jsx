3858c4e173369c9aff744f3578ab63a75ff3c152
Kirillova <keira.kirillova@gmail.com>
2020-04-13 17:50:38 +0300

    add request to movieVideos tab component

--- src/components/pages/MoviePage/MoviePage.jsx
+++ src/components/pages/MoviePage/MoviePage.jsx
@@ -16,7 +16,6 @@ class MoviePage extends React.Component {
 
     this.state = {
       movieDetail: {},
-      movieVideos: [],
       movieCredits: [],
       isLoading: true
     }
@@ -31,32 +30,25 @@ class MoviePage extends React.Component {
       .then(data => this.setState({movieDetail: data, isLoading: false}))
   };
 
-  getMovieVideos = (id) => {
-    CallApi.get(`/movie/${id}/videos`)
-      .then(data => this.setState({movieVideos: data.results}))
-  };
-
   getMovieCredits = (id) => {
     CallApi.get(`/movie/${id}/credits`)
       .then(data => this.setState({movieCredits: data.cast}))
   };
 
   componentDidMount() {
-    const {id} = this.props.match.params;
-    this.getMovieDetail(id);
-    this.getMovieVideos(id);
-    this.getMovieCredits(id);
+    const {movie_id} = this.props.match.params;
+    this.getMovieDetail(movie_id);
+    this.getMovieCredits(movie_id);
   }
 
   render() {
     const {
       isLoading,
       movieDetail,
-      movieVideos,
       movieCredits
     } = this.state;
 
-    const {id} = this.props.match.params;
+    const {movie_id} = this.props.match.params;
 
     return (
       <>
@@ -65,21 +57,21 @@ class MoviePage extends React.Component {
             <div className="row mt-5">
               <MovieInfo
                 movieDetail={movieDetail}
-                movie_id={id}
+                movie_id={movie_id}
               />
             </div>
             <div className="row mt-5">
               <div className="col-12">
                 <Nav tabs>
-                  <TabsNavigation />
+                  <TabsNavigation movie_id={movie_id}/>
                 </Nav>
                 <TabContent>
                   <TabPane>
                     <div className="row mt-5 mb-5">
                       <Switch>
-                        <Route exact path="/movie/:id/detail"><MovieDetail movieDetail={movieDetail}/></Route>
-                        <Route path="/movie/:id/videos"><MovieVideos movieVideos={movieVideos}/></Route>
-                        <Route path="/movie/:id/credits"><MovieCredits movieCredits={movieCredits}/></Route>
+                        <Route exact path="/movie/:movie_id/detail"><MovieDetail movieDetail={movieDetail}/></Route>
+                        <Route path="/movie/:movie_id/videos" component={MovieVideos} />
+                        <Route path="/movie/:movie_id/credits"><MovieCredits movieCredits={movieCredits}/></Route>
                       </Switch>
                     </div>
                   </TabPane>
--- src/components/pages/MoviePage/Tabs/MovieVideos.jsx
+++ src/components/pages/MoviePage/Tabs/MovieVideos.jsx
@@ -1,21 +1,50 @@
 import React from 'react';
+import CallApi from "../../../../api/api";
+import {Loader} from "../../../UIComponents/Loader";
 
-const MovieVideos = ({movieVideos}) => {
-  return (
-    <>
-      {movieVideos.length > 0 ?
-        movieVideos.map(item => {
-        return <div className="col-4 mb-5" key={item.id}>
-          <iframe width="100%"
-                  height="200px"
-                  src={`https://www.youtube.com/embed/${item.key}`} frameBorder="0"
-                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen title={item.name}/>
-        </div>
-        }) : <p>К сожалению, видео отсутствует.</p>
-      }
-    </>
-  )
-};
+class MovieVideos extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movieVideos: [],
+      isLoading: true
+    }
+  }
+
+  getMovieVideos = (id) => {
+    CallApi.get(`/movie/${id}/videos`)
+      .then(data => this.setState({movieVideos: data.results, isLoading: false}))
+  };
+
+  componentDidMount() {
+    const {movie_id} = this.props.match.params;
+    this.getMovieVideos(movie_id);
+  }
+
+  render() {
+    const {movieVideos, isLoading} = this.state;
+
+    return (
+      <>
+        {isLoading ? <Loader /> :
+          <>
+            {movieVideos.length > 0 ?
+              movieVideos.map(item => {
+                return <div className="col-4 mb-5" key={item.id}>
+                  <iframe width="100%"
+                          height="200px"
+                          src={`https://www.youtube.com/embed/${item.key}`} frameBorder="0"
+                          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                          allowFullScreen title={item.name}/>
+                </div>
+              }) : <p>К сожалению, видео отсутствует.</p>
+            }
+          </>
+        }
+      </>
+    )
+  }
+}
 
 export default MovieVideos;
