b60c8dea071b4415f39f39dafec58311f531471f
Kirillova <keira.kirillova@gmail.com>
2020-04-21 19:10:04 +0300

    install react-redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./stylesheets/index.css";
+import {Provider} from "react-redux";
 import App from "./components/App";
 import store from './store/store';
 
-ReactDOM.render(<App store={store}/>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"));
