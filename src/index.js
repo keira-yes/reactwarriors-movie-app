64d9c4c642d61327fb2acf7e3f43c8c01e05c3fe
Kirillova <keira.kirillova@gmail.com>
2020-04-21 14:49:14 +0300

    install redux, add initial action creator, reducer and store

--- src/index.js
+++ src/index.js
@@ -3,5 +3,33 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./stylesheets/index.css";
+import {createStore} from "redux";
+
+const actionCreatorUpdateUser = (payload) => {
+  return {
+    type: 'UPDATE_USER',
+    payload
+  }
+};
+
+const initialState = {
+  user: null
+};
+
+const reducerApp = (state = initialState, action) => {
+  console.log('reducerApp', state, action);
+  switch (action.type) {
+    case 'UPDATE_USER':
+      return {...state, user: action.payload};
+    default:
+      return state
+  }
+};
+
+const store = createStore(reducerApp);
+
+store.dispatch(actionCreatorUpdateUser({name: "Irina"}));
+
+console.log('store after update', store.getState());
 
 ReactDOM.render(<App />, document.getElementById("root"));
