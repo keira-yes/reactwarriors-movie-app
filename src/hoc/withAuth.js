29ed385f4c46cb1caaaaae45a19b82b9e40f4d56
Kirillova <keira.kirillova@gmail.com>
2020-04-23 18:02:44 +0300

    add withAuth hoc

---
+++ src/hoc/withAuth.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {bindActionCreators} from "redux";
+import * as authActions from "../redux/auth/auth.actions";
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+};
+
+const mapDispatchToProps = dispatch => ({
+  authActions: bindActionCreators(authActions, dispatch)
+});
+
+export const withAuth = ComponentName => connect(mapStateToProps, mapDispatchToProps)(
+  class WithAuth extends React.Component {
+    render() {
+      return <ComponentName {...this.props} />
+    }
+  }
+);
