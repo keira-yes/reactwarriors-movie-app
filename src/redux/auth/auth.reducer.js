9282ed7167c7ef5dd1e94cd4eaf4f8b4b29ee9f7
Kirillova <keira.kirillova@gmail.com>
2020-04-23 16:12:31 +0300

    add update and delete cookies to middleware to make clean reducer

--- src/redux/auth/auth.reducer.js
+++ src/redux/auth/auth.reducer.js
@@ -1,7 +1,5 @@
-import Cookies from 'universal-cookie';
 import * as types from './auth.types';
-
-const cookies = new Cookies();
+import {cookies} from '../../utils/cookies'
 
 const initialState = {
   user: null,
@@ -16,13 +14,8 @@ const authReducer = (state = initialState, action) => {
     case types.UPDATE_USER:
       return {...state, user: action.payload};
     case types.UPDATE_SESSION_ID:
-      cookies.set('session_id', action.payload, {
-        path: '/',
-        maxAge: 2592000
-      });
       return {...state, session_id: action.payload};
     case types.LOGOUT:
-      cookies.remove('session_id');
       return {
         ...state,
         user: null,
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,25 @@ import {applyMiddleware, createStore} from "redux";
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from "./rootReducer";
+import {UPDATE_SESSION_ID, LOGOUT} from "./auth/auth.types";
+import {cookies} from "../utils/cookies";
+
+const updateCookies = store => next => action => {
+  if (action.type === UPDATE_SESSION_ID) {
+    cookies.set('session_id', action.payload, {
+      path: '/',
+      maxAge: 2592000
+    });
+  }
+  if (action.type === LOGOUT) {
+    cookies.remove('session_id');
+  }
+  return next(action)
+};
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(thunk, updateCookies))
 );
 
 export default store;
