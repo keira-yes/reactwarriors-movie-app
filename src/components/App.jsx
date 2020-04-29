6300fc60f7de0d43292afb129cddddbe5d65ce6f
Kirillova <keira.kirillova@gmail.com>
2020-04-29 16:26:05 +0300

    remove context from App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,107 +1,45 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { connect } from 'react-redux';
 import { Modal, ModalBody } from "reactstrap";
 import Header from './Header/Header';
 import LoginForm from "./Header/Login/LoginForm";
 import MoviesPage from "./pages/MoviesPage/MoviesPage";
 import MoviePage from "./pages/MoviePage/MoviePage";
-import {
-  updateUser,
-  updateSessionId,
-  onLogout,
-  updateFavoriteMovies,
-  updateWatchListMovies,
-  toggleModal,
-  fetchAuth,
-  fetchFavoriteMovies,
-  fetchWatchListMovies
-} from "../redux/auth/auth.actions";
-
-export const AppContext = React.createContext();
+import {withAuth} from "../hoc/withAuth";
 
 class App extends React.Component {
 
   componentDidMount() {
-    const {session_id, fetchAuth} = this.props;
-    if (session_id) {
-      fetchAuth(session_id)
+    const {auth, authActions} = this.props;
+    if (auth.session_id) {
+      authActions.fetchAuth(auth.session_id)
     }
   }
 
   componentDidUpdate(prevProps) {
-    const {user, session_id, fetchFavoriteMovies, fetchWatchListMovies} = this.props;
-    if (!prevProps.user && user) {
-      fetchFavoriteMovies(user, session_id);
-      fetchWatchListMovies(user, session_id);
+    const {auth, authActions} = this.props;
+    if (!prevProps.auth.user && auth.user) {
+      authActions.fetchFavoriteMovies(auth.user, auth.session_id);
+      authActions.fetchWatchListMovies(auth.user, auth.session_id);
     }
   }
 
   render() {
-
-    const {
-      user,
-      session_id,
-      updateUser,
-      updateSessionId,
-      onLogout,
-      favoriteList,
-      watchList,
-      showLoginModal,
-      toggleModal
-    } = this.props;
+    const {auth, authActions} = this.props;
 
     return (
       <Router>
-        <AppContext.Provider value={{
-          user,
-          updateUser,
-          session_id,
-          updateSessionId,
-          onLogout,
-          favoriteList,
-          getFavoriteMovies: fetchFavoriteMovies,
-          watchList,
-          getWatchListMovies: fetchWatchListMovies,
-          showLoginModal,
-          toggleModal
-        }}>
-          <>
-            <Header/>
-            <Modal isOpen={showLoginModal} toggle={toggleModal}>
-              <ModalBody>
-                <LoginForm toggleModal={toggleModal}/>
-              </ModalBody>
-            </Modal>
-            <Route exact path="/" component={MoviesPage} />
-            <Route path="/movie/:movie_id/:tab" component={MoviePage} />
-          </>
-        </AppContext.Provider>
+        <Header/>
+        <Modal isOpen={auth.showLoginModal} toggle={authActions.toggleModal}>
+          <ModalBody>
+            <LoginForm toggleModal={authActions.toggleModal}/>
+          </ModalBody>
+        </Modal>
+        <Route exact path="/" component={MoviesPage} />
+        <Route path="/movie/:movie_id/:tab" component={MoviePage} />
       </Router>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    user: state.auth.user,
-    session_id: state.auth.session_id,
-    favoriteList: state.auth.favoriteList,
-    watchList: state.auth.watchList,
-    showLoginModal: state.auth.showLoginModal
-  }
-};
-
-const mapDispatchToProps = {
-  updateUser,
-  updateSessionId,
-  onLogout,
-  updateFavoriteMovies,
-  updateWatchListMovies,
-  toggleModal,
-  fetchAuth,
-  fetchFavoriteMovies,
-  fetchWatchListMovies
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default withAuth(App);
